5c08ea929bc7bdf13e0909b38719ff9d655e519d
Andrea Ridi <andrea.ridi@gmail.com>
2024-09-18 12:19:19 -0400

    Secrets in a toml file

--- supabase_client.py
+++ supabase_client.py
@@ -1,14 +1,16 @@
 import os
 from supabase import create_client, Client
-from dotenv import load_dotenv
+import streamlit as st
+#from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+#load_dotenv()
 
 def init_supabase() -> Client:
-    url: str = os.environ.get("SUPABASE_URL")
-    key: str = os.environ.get("SUPABASE_KEY")
-    supabase: Client = create_client(url, key)
+    #url: str = os.environ.get("SUPABASE_URL")
+    #key: str = os.environ.get("SUPABASE_KEY")
+    
+    supabase: Client = create_client(st.secrets["SUPABASE_URL"], st.secrets["SUPABASE_KEY"])
     return supabase
 
 def sign_up(supabase: Client, email: str, password: str):
